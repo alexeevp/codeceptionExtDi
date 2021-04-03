4051ad04bd105ae4bdc63f6e6a4ff9611ea4e3d3
Alex <apirogoff@gmail.com>
2021-04-03 12:49:17 +0300

    Create CodeceptionExtDi.php

---
+++ CodeceptionExtDi.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace tests\_support\extensions;
+
+use Codeception\Event\TestEvent;
+use Codeception\Events;
+use Codeception\Extension;
+use Yiisoft\Arrays\ArrayHelper;
+use Yiisoft\Di\Container;
+use Yiisoft\Injector\Injector;
+
+/**
+ * Class CodeceptionExtDI
+ * Расширение для Codeception
+ * Позволяет внедрять зависимости используя конфигурации проекта
+ * Пути к конфигурациям прописываются в месте объявления расширения
+ * extensions:
+ *   enabled:
+ *       - tests\_support\extensions\CodeceptionExtDI
+ *   config:
+ *       tests\_support\extensions\CodeceptionExtDI:
+ *           - ./config/di/definitions.php
+ *           - definitions.php
+ * @package tests\_support\extensions
+ */
+class CodeceptionExtDI extends Extension
+{
+    /**
+     * Метод посредством которого имеется возможность внедрять зависимости
+     * @var string
+     */
+    protected $injectMethodName = '_dependencies';
+
+    protected static array $events = [
+        Events::TEST_BEFORE => 'injectDependencies',
+    ];
+
+    private $container;
+
+    /**
+     * @param TestEvent $event
+     * @throws \ReflectionException
+     * @throws \Yiisoft\Factory\Exceptions\InvalidConfigException
+     */
+    public function injectDependencies(TestEvent $event): void
+    {
+        $test = $event->getTest();
+
+        if (!method_exists($test, $this->injectMethodName)) {
+            return;
+        }
+
+        $injector = new Injector($this->getContainer());
+        $injector->invoke([$test, $this->injectMethodName]);
+    }
+
+
+    /**
+     * @return Container
+     * @throws \Yiisoft\Factory\Exceptions\InvalidConfigException
+     */
+    private function getContainer(): Container
+    {
+        if (is_object($this->container)) {
+            return $this->container;
+        }
+
+        $configs = array_map(fn($path) => require_once $path, $this->config);
+        $definitions = ArrayHelper::merge(...$configs);
+
+        return $this->container = new Container($definitions);
+    }
+
+}
